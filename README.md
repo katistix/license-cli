c50652be838899a0148b328d1d4a03b9e69acfdf
Paul <2018.tpg@gmail.com>
2023-11-04 14:12:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## katistix/license-cli
+
+This is a CLI tool to help you generate a license file for your project.
+
+### Installation
+
+As of now, this is only available on Unix-like systems. I will add support for Windows in the future.
+
+To install this tool, run the following command in your terminal:
+
+```bash
+curl -s https://raw.githubusercontent.com/katistix/license-cli/master/install.sh | bash
+```
+
+### Usage
+
+To use this tool, run the following command in your terminal:
+
+```bash
+license add <license_type>
+```
+
+Replace `<license_type>` with the license you want to use. A list of supported licenses can be found in the `/licenses` folder.
+
+Example to add the MIT license to your project:
+
+```bash
+license add mit
+```
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set the repository details
+REPO="katistix/license-cli"
+TOOL_NAME="license"
+
+# Determine the latest release version
+latest_release_url="https://api.github.com/repos/$REPO/releases/latest"
+latest_release_version=$(curl -s $latest_release_url | grep -oP '"tag_name": "\K(.*)(?=")')
+
+# Define the URL for the release archive
+release_url="https://github.com/$REPO/releases/download/$latest_release_version/$TOOL_NAME-$latest_release_version.tar.gz"
+
+# Define the installation directory
+INSTALL_DIR="/usr/local/bin"
+
+# Download and extract the release archive
+curl -LO $release_url
+tar -xzvf $TOOL_NAME-$latest_release_version.tar.gz
+
+# Make the tool executable and move it to the installation directory
+chmod +x $TOOL_NAME
+mv $TOOL_NAME $INSTALL_DIR/
+
+# Clean up downloaded files
+rm $TOOL_NAME-$latest_release_version.tar.gz
+
+# Provide a success message
+echo "$TOOL_NAME $latest_release_version has been installed to $INSTALL_DIR"
